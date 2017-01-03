e61c503434ed6fd56699da9972368873471c53e8
GalihBaskara <1galihbaskara@gmail.com>
2017-01-04 06:30:48 +0700

    add files

---
+++ ambiltegangan/adc/adc.ino
@@ -0,0 +1,20 @@
+#define sensor A7
+
+int adc = 0;
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReadResolution(12);
+  pinMode(A7, INPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  adc = analogRead(A7);
+  Serial.println(adc);
+  delay(500);
+}
---
+++ ambiltegangan/ambiltegangan.ino
@@ -0,0 +1,23 @@
+#define pwm 11
+int i = 0;
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pwm , OUTPUT);
+}
+
+void loop()
+{
+  for(i = 0; i < 255 ; i +=1)
+  {
+    analogWrite(pwm,i);
+    Serial.println(i);
+    delay(100);
+  }
+Serial.println(i);
+
+}
+
